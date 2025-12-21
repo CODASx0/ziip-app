c26c26b553846daea93fe8bc71b6a2a0fed1e6ca
kEVIN <codas0327@gmail.com>
2025-12-21 21:06:34 +0800

    Initial Commit

---
+++ Ziip/ZiipApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZiipApp.swift
+//  Ziip
+//
+//  Created by Kevin on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ZiipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
